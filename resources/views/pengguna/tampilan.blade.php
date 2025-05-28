2516a47c36ab83539f78f1aa29fadc116f387388
afu manan <afumanan07@gmail.com>
2025-05-28 11:42:34 +0700

    tes

--- resources/views/pengguna/tampilan.blade.php
+++ resources/views/pengguna/tampilan.blade.php
@@ -4,9 +4,19 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>Beranda</title>
 </head>
 <body>
-    beranda pengguna
+    <h2>test hosting AND Boostrap</h2>
+
+    <div class="row">
+  <div class="col">
+    <input type="text" class="form-control" placeholder="First name" aria-label="First name">
+  </div>
+  <div class="col">
+    <input type="text" class="form-control" placeholder="Last name" aria-label="Last name">
+  </div>
+</div>
+
 </body>
 </html>
